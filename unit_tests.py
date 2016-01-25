91d101aa56ebbace2670d76a6b59f2563641e404
Dan Bordeanu <danbordeanu@gmail.com>
2016-01-25 23:06:37 +0100

    - Unit test for config file

--- unit_tests.py
+++ unit_tests.py
@@ -3,7 +3,7 @@ __author__ = 'danbordeanu'
 import unittest
 
 from get_md5 import get_md5
-
+from config_parser import check_if_config_exists
 
 class Md5Test(unittest.TestCase):
     def test_is_there_md5(self):
@@ -13,6 +13,12 @@ class Md5Test(unittest.TestCase):
         """
         self.assertTrue(get_md5(), 'Uhh, md5 not working')
 
+    def test_is_there_config(self):
+        """
+        Test if there is config file
+        :return:
+        """
+        self.assertTrue(check_if_config_exists('mimi'), 'Uhhh, no config file')
 
 if __name__ == '__main__':
     unittest.main()
