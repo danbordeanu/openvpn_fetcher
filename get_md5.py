622de51b30d4b588ad50b6e33413a497837b8fe8
Dan Bordeanu <danbordeanu@gmail.com>
2016-02-24 22:50:56 +0100

    - if there is no config file start download

--- get_md5.py
+++ get_md5.py
@@ -12,11 +12,15 @@ def get_md5():
     """
     openvpn_config = parser.config_params('openvpnconfig')['file']
     md5 = hashlib.md5()
-    f = open(openvpn_config)
-    for line in f:
-        md5.update(line)
-    f.close()
-    return md5.hexdigest()
+    try:
+        f = open(openvpn_config)
+        for line in f:
+            md5.update(line)
+        f.close()
+        return md5.hexdigest()
+    except:
+        logger_settings.logger.info('There is no config file...will download one for you')
+        return md5.hexdigest()
 
 config_md5 = get_md5()
 logger_settings.logger.info('md5 of local openvpn config is:%s' %config_md5)
