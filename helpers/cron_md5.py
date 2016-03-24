0bec6335f927ab28f7b354dc539d093cb383a0cc
Dan Bordeanu <danbordeanu@gmail.com>
2016-03-24 23:11:23 +0100

    - try/except in cron job function

--- helpers/cron_md5.py
+++ helpers/cron_md5.py
@@ -7,7 +7,6 @@
 
 __author__ = 'dan bordeanu'
 
-import os
 import hashlib
 
 config_file = 'client_danbordeanu'
@@ -19,16 +18,15 @@ def cron_md5():
 
     :rtype : object
     """
-    if os.path.isfile(config_file):
+    try:
         with open(config_file) as file_to_check:
             data = file_to_check.read()
             md5_returned = hashlib.md5(data).hexdigest()
             assert isinstance(md5_returned, object)
-            with open(md5_sum_value, 'w') as text_file:
-                text_file.write('{0}'.format(md5_returned))
-    else:
-        print 'no file'
-        exit()
+        with open(md5_sum_value, 'w') as text_file:
+            text_file.write('{0}'.format(md5_returned))
+    except IOError as e:
+        print 'error creating md5 {0}'.format(e)
 
 cron_md5()
 
