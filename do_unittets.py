572f850874158c742c973247503b052618417931
Dan Bordeanu <danbordeanu@gmail.com>
2016-03-24 19:57:01 +0100

    - unit test for config.ini file

---
+++ do_unittets.py
@@ -0,0 +1,19 @@
+__author__ = 'danbordeanu'
+
+import unittest
+
+from get_md5 import get_md5
+from config_parser import check_if_config_exists
+
+
+class GetAllTest(unittest.TestCase):
+    def test_md5(self):
+        self.assertTrue(get_md5)
+
+    def Test_config(self):
+        self.assertTrue(check_if_config_exists('config.ini'))
+
+
+if __name__ == 'main':
+    suite = unittest.TestLoader().loadTestsFromTestCase(GetAllTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
